828f282f730fb0f3e0b17c33829450f4ba809687
Tudor <126792536+tudorcuc@users.noreply.github.com>
2024-04-01 11:12:41 +0300

    Delete Springpro directory

--- Springpro/src/main/java/com/Springpro/Springpro/Repository/SongRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Springpro.Springpro.Repository;
-
-import com.Springpro.Springpro.Entity.Song;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SongRepository extends JpaRepository<Song, Integer> {
-}
